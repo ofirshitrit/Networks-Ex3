4ac09973b4d35b5cf48c69fca31c026706fd60d1
ofirshitrit <ofirha18@gmail.com>
2023-05-04 14:57:35 +0300

    first commit

---
+++ Receiver.py
@@ -0,0 +1,111 @@
+import socket
+import time
+
+# Define the IP address and port number for the receiver
+HOST = 'localhost'
+PORT = 8080
+HALF_SIZE = 1310720
+
+# Create a socket object
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print("Create a socket succeed")
+
+# Bind the socket to a specific address and port number
+sock.bind((HOST, PORT))
+print("Bind succeed")
+
+# Listen for incoming connections
+sock.listen()
+print("ready to receive files")
+
+# Wait for a connection
+conn, addr = sock.accept()
+print(f'Connected by {addr}')
+
+# Receive the first part of the file
+received_data = b''
+_sum = 0
+while True:
+    data = conn.recv(1024)
+    _sum += len(data)
+    if _sum == HALF_SIZE:
+        break
+    received_data += data
+print("get the first data 1111")
+start_time = time.time()  # start measuring time
+
+# Save the time it took to receive the first part
+part1_time = time.time() - start_time
+print("the time: ", part1_time)
+
+# Send back an authentication message
+conn.sendall(b'Authentication successful')
+print("Authentication successful")
+
+# # Change the CC Algorithm to cubic
+# conn.sendall(b'CUBIC')
+# print("*** Algo: cubic ***")
+
+# Receive the second part of the file
+data = conn.recv(1310720)  # receive up to half of the data
+start_time = time.time()  # start measuring time
+
+# Save the time it took to receive the second part
+part2_time = time.time() - start_time
+print("get the second data 2222")
+
+# Check if the sender wants to send the file again
+while True:
+    # Ask the user if they want to send the file again
+    user_input = input('Do you want to receive the file again? (y/n): ')
+
+    if user_input.lower() == 'y':
+        # Send a notification to the sender that the receiver is ready for another transfer
+        conn.sendall(b'Authentication successful')
+
+        # Change the CC Algorithm back to reno
+        sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_CONGESTION, 'reno'.encode())
+        print("*** Algo: reno ***")
+
+        # Receive the first part of the file
+        data = conn.recv(1310720)  # receive up to half of the data
+        print("Receive the first part 1111")
+        start_time = time.time()  # start measuring time
+
+        # Save the time it took to receive the first part
+        part1_time += time.time() - start_time
+
+        # Change the CC Algorithm to cubic
+        sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_CONGESTION, 'cubic'.encode())
+        print("*** Algo: cubic ***")
+
+        # Receive the second part of the file
+        data = conn.recv(1048576)  # receive up to 1 MB of data
+        print("Receive the second part 2222")
+        start_time = time.time()  # start measuring time
+
+        # Save the time it took to receive the second part
+        part2_time += time.time() - start_time
+
+    elif user_input.lower() == 'n':
+        # Send an exit message to the sender
+        conn.sendall(b'GoodBy')
+
+        # Print out the times for each part of the file
+        print('Time to receive first part:', part1_time)
+        print('Time to receive second part:', part2_time)
+
+        # Calculate the average time for each part of the file
+        avg_part1_time = part1_time / 5
+        avg_part2_time = part2_time / 5
+
+        # Print the average time
+        print('Average time to receive first part:', avg_part1_time)
+        print('Average time to receive second part:', avg_part2_time)
+
+        # Close the connection
+        conn.close()
+        break
+
+    else:
+        print('Invalid input. Please enter y or n.')
---
+++ Sender.py
@@ -0,0 +1,84 @@
+import socket
+
+# read the file
+filename = 'file.txt'
+with open(filename, 'r') as f:
+    file_data = f.read()
+    print("file is open")
+
+# set up TCP connection
+server_IP_address = 'localhost'
+server_port = 8080
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print("socket created")
+try:
+    s.connect((server_IP_address, server_port))
+    print("connection established")
+except socket.error as err:
+    print("Connection failed with error %s" % err)
+    s.close()
+    exit()
+
+# send first half of the file
+data_size = len(file_data)
+half_size = data_size // 2
+first_half = file_data[:half_size]
+s.sendall(first_half.encode())
+print("first time of sending the data 1111")
+
+# wait for authentication from receiver
+print("wait for authentication ")
+auth = s.recv(1024)
+print("get authentication: ", auth)
+if auth != b'Authentication successful':  # TODO - change this to xor of 9150 ^ 4966
+    print('Authentication failed')
+    s.close()
+else:
+    print('Authentication successful')
+
+# switch to cubic congestion control algorithm
+s.setsockopt(socket.IPPROTO_TCP, socket.TCP_CONGESTION, 'cubic'.encode())
+print("*** Algo: cubic ***")
+# send second half of the file
+second_half = file_data[half_size:]
+s.sendall(second_half.encode())
+print("sent the data in the second time 2222")
+
+# ask user if they want to send the file again
+send_again = input('Send the file again? (y/n): ')
+while send_again.lower() == 'y':
+    # notify receiver
+    s.sendall(b'Send again')
+
+    # switch back to reno congestion control algorithm
+    s.setsockopt(socket.IPPROTO_TCP, socket.TCP_CONGESTION, 'reno'.encode())
+    print("*** Algo: reno ***")
+
+    # send first half of the file again
+    s.sendall(first_half.encode())
+    print("send the first time 1111")
+
+    # wait for authentication from receiver
+    auth = s.recv(1024)
+    print("auth: ", auth)
+    if auth != b'Authentication successful':
+        print('Authentication failed')
+        s.close()
+    else:
+        print('Authentication successful')
+
+    # switch to cubic congestion control algorithm
+    s.setsockopt(socket.IPPROTO_TCP, socket.TCP_CONGESTION, 'cubic'.encode())
+
+    # send second half of the file again
+    s.sendall(second_half.encode())
+    print("send the second time")
+
+    # ask user if they want to send the file again
+    send_again = input('Send the file again? (y/n): ')
+
+# send exit message
+s.sendall(b'GoodBy')
+
+# close the connection
+s.close()
