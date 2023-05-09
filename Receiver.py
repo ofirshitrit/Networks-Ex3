bbdb4d768d4300cbb415ed015f8e9d0714155cf5
ofirshitrit <ofirha18@gmail.com>
2023-05-09 14:51:31 +0300

    Revert "change the code"
    This reverts commit 2a7e1a2828d6a6a7f7db16abfc8bdd568874ae37.

--- Receiver.py
+++ Receiver.py
@@ -22,12 +22,7 @@ print("ready to receive files")
 conn, addr = sock.accept()
 print(f'Connected by {addr}')
 
-# Change the CC Algorithm back to reno
-sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_CONGESTION, 'reno'.encode())
-print("*** change Algo: RENO ***")
-
 # Receive the first part of the file
-start_time = time.time()  # start measuring time
 received_data = b''
 _sum = 0
 while True:
@@ -37,27 +32,26 @@ while True:
         break
     received_data += data
 print("### Receive the 1st data  ### ")
+start_time = time.time()  # start measuring time
+
+# Save the time it took to receive the first part
 part1_time = time.time() - start_time
+print("the time: ", part1_time)
 
 # Send back an authentication message
 xor_ans = 1101011111001001  # 9150 ^ 4699 = 10001110111110 ^ 1001001011011 = 1101011111001001
 conn.sendall(b'xor_ans')
 print("Authentication successful")
 
-# Change the CC Algorithm to cubic
-sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_CONGESTION, 'cubic'.encode())
-print("*** Algo: CUBIC ***")
+# # Change the CC Algorithm to cubic
+# conn.sendall(b'CUBIC')
+# print("*** Algo: cubic ***")
 
 # Receive the second part of the file
+data = conn.recv(1310720)  # receive up to half of the data
 start_time = time.time()  # start measuring time
-received_data = b''
-_sum = 0
-while True:
-    data = conn.recv(1024)
-    _sum += len(data)
-    if _sum == HALF_SIZE:
-        break
-    received_data += data
+
+# Save the time it took to receive the second part
 part2_time = time.time() - start_time
 print("### Receive the 2nd data  ### ")
 
@@ -73,42 +67,32 @@ while True:
 
         # Change the CC Algorithm back to reno
         sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_CONGESTION, 'reno'.encode())
-        print("*** change Algo: RENO ***")
+        print("*** Algo: reno ***")
 
         # Receive the first part of the file
-        start_time = time.time()  # start measuring time
-        received_data = b''
-        _sum = 0
-        while True:
-            data = conn.recv(1024)
-            _sum += len(data)
-            if _sum == HALF_SIZE:
-                break
-            received_data += data
+        data = conn.recv(1310720)  # receive up to half of the data
         print("### Receive the 1st data  ### ")
+        start_time = time.time()  # start measuring time
+
+        # Save the time it took to receive the first part
         part1_time += time.time() - start_time
 
         # Change the CC Algorithm to cubic
         sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_CONGESTION, 'cubic'.encode())
-        print("*** Algo: CUBIC ***")
+        print("*** Algo: cubic ***")
 
         # Receive the second part of the file
+        data = conn.recv(1048576)  # receive up to 1 MB of data
+        print("### Receive the 1st data  ### ")
         start_time = time.time()  # start measuring time
-        received_data = b''
-        _sum = 0
-        while True:
-            data = conn.recv(1024)
-            _sum += len(data)
-            if _sum == HALF_SIZE:
-                break
-            received_data += data
-        print("### Receive the 2st data  ### ")
+
+        # Save the time it took to receive the second part
         part2_time += time.time() - start_time
 
     elif user_input.lower() == 'n':
         # Print out the times for each part of the file
-        print('Time to receive first part with RENO:', part1_time)
-        print('Time to receive second part with CUBIC:', part2_time)
+        print('Time to receive first part:', part1_time)
+        print('Time to receive second part:', part2_time)
 
         # Calculate the average time for each part of the file
         avg_part1_time = part1_time / 5
